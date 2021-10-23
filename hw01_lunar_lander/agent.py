4ce65bffbc21b2415d107a6763817cce7bf1160a
IMDxD <nestor969@mail.ru>
2021-10-23 17:42:57 +0300

    good agent

--- hw01_lunar_lander/agent.py
+++ hw01_lunar_lander/agent.py
@@ -1,6 +1,4 @@
-import random
 import numpy as np
-import os
 import torch
 
 
@@ -10,9 +8,9 @@ DEVICE = torch.device("cuda")
 class Agent:
     def __init__(self):
         self.model = torch.load(__file__[:-8] + "/agent.pkl")
-        
+        self.model.eval()
+
     def act(self, state):
         state = torch.tensor(np.array(state)).view(1, -1).to(DEVICE)
         action_rewards = self.model(state).squeeze(0).detach().cpu().numpy()
         return np.argmax(action_rewards)
-
