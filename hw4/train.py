06a7db51122cb70d72a249e4f3a7300c8aca2338
IMDxD <nestor969@mail.ru>
2021-12-08 22:37:15 +0300

    world model

--- hw4/train.py
+++ hw4/train.py
@@ -1,12 +1,12 @@
 import copy
+import random
+from collections import deque
 
 import numpy as np
 import torch
-import tqdm
 from torch import nn
 from torch.nn import functional as F
 from torch.optim import Adam
-from torch.utils.data import DataLoader, Dataset
 
 GAMMA = 0.99
 TAU = 0.002
@@ -16,31 +16,12 @@ CRITIC_LR = 5e-4
 ACTOR_LR = 2e-4
 DEVICE = "cuda"
 BATCH_SIZE = 128
-ENV_NAME = "AntBulletEnv-v0"
-TRANSITIONS = 2_000_000
+TRANSITIONS = 1_000_000
 
 
 def soft_update(target, source):
     for tp, sp in zip(target.parameters(), source.parameters()):
         tp.data.copy_((1 - TAU) * tp.data + TAU * sp.data)
-
-
-class TransitionDataset(Dataset):
-    def __getitem__(self, index):
-        state, action, next_state, reward, done = self.transitions[index]
-        return (
-            torch.tensor(state, dtype=torch.float32),
-            torch.tensor(action, dtype=torch.float32),
-            torch.tensor(next_state, dtype=torch.float32),
-            torch.tensor([reward], dtype=torch.float32),
-            torch.tensor([done], dtype=torch.float32),
-        )
-
-    def __init__(self, data):
-        self.transitions = data
-
-    def __len__(self):
-        return len(self.transitions)
 
 
 class Actor(nn.Module):
@@ -52,7 +33,7 @@ class Actor(nn.Module):
             nn.Linear(256, 256),
             nn.ELU(),
             nn.Linear(256, action_dim),
-            nn.Tanh(),
+            nn.Tanh()
         )
 
     def forward(self, state):
@@ -67,7 +48,7 @@ class Critic(nn.Module):
             nn.ELU(),
             nn.Linear(256, 256),
             nn.ELU(),
-            nn.Linear(256, 1),
+            nn.Linear(256, 1)
         )
 
     def forward(self, state, action):
@@ -75,7 +56,7 @@ class Critic(nn.Module):
 
 
 class TD3:
-    def __init__(self, state_dim, action_dim, alpha=1e-1):
+    def __init__(self, state_dim, action_dim):
         self.actor = Actor(state_dim, action_dim).to(DEVICE)
         self.critic_1 = Critic(state_dim, action_dim).to(DEVICE)
         self.critic_2 = Critic(state_dim, action_dim).to(DEVICE)
@@ -87,51 +68,56 @@ class TD3:
         self.target_actor = copy.deepcopy(self.actor)
         self.target_critic_1 = copy.deepcopy(self.critic_1)
         self.target_critic_2 = copy.deepcopy(self.critic_2)
-        self._alpha = alpha
 
-    def update(self, batch):
+        self.replay_buffer = deque(maxlen=200000)
 
-        state, action, next_state, reward, done = batch
-        state = state.to(DEVICE)
-        action = action.to(DEVICE)
-        next_state = next_state.to(DEVICE)
-        reward = reward.to(DEVICE)
-        done = done.to(DEVICE)
+    def update(self, transition):
+        self.replay_buffer.append(transition)
+        if len(self.replay_buffer) > BATCH_SIZE * 16:
+            # Sample batch
+            transitions = [self.replay_buffer[random.randint(0, len(self.replay_buffer) - 1)] for _ in
+                           range(BATCH_SIZE)]
+            state, action, next_state, reward, done = zip(*transitions)
+            state = torch.tensor(np.array(state), device=DEVICE, dtype=torch.float)
+            action = torch.tensor(np.array(action), device=DEVICE, dtype=torch.float)
+            next_state = torch.tensor(np.array(next_state), device=DEVICE, dtype=torch.float)
+            reward = torch.tensor(np.array(reward), device=DEVICE, dtype=torch.float)
+            done = torch.tensor(np.array(done), device=DEVICE, dtype=torch.float)
 
-        with torch.no_grad():
+            # Update actor
 
-            noise = torch.randn_like(action).to(DEVICE)
-            next_actor_action = self.target_actor(next_state)
-            next_actor_action = torch.clamp(
-                next_actor_action + EPS * noise, -ACTOR_CLIP, ACTOR_CLIP
-            )
-            target_q1 = self.target_critic_1(next_state, next_actor_action)
-            target_q2 = self.target_critic_2(next_state, next_actor_action)
-            target_q = torch.min(target_q1, target_q2)
-            target_q = reward.squeeze(1) + (1 - done.squeeze(1)) * GAMMA * target_q
+            with torch.no_grad():
 
-        self.critic_1_optim.zero_grad()
-        self.critic_2_optim.zero_grad()
-        self.actor_optim.zero_grad()
+                noise = torch.randn_like(action).to(DEVICE)
+                next_actor_action = self.target_actor(next_state)
+                next_actor_action = torch.clamp(next_actor_action + EPS * noise, -ACTOR_CLIP, ACTOR_CLIP)
+                target_q1 = self.target_critic_1(next_state, next_actor_action)
+                target_q2 = self.target_critic_2(next_state, next_actor_action)
+                target_q = torch.min(target_q1, target_q2)
+                target_q = reward + (1 - done) * GAMMA * target_q
 
-        actor_action = self.actor(state)
-        current_q1 = self.critic_1(state, action)
-        current_q2 = self.critic_2(state, action)
-        loss_critic_1 = F.mse_loss(current_q1, target_q)
-        loss_critic_2 = F.mse_loss(current_q2, target_q)
-        loss_critic_1.backward()
-        loss_critic_2.backward()
+            self.critic_1_optim.zero_grad()
+            self.critic_2_optim.zero_grad()
+            self.actor_optim.zero_grad()
 
-        self.critic_1_optim.step()
-        self.critic_2_optim.step()
+            actor_action = self.actor(state)
+            current_q1 = self.critic_1(state, action)
+            current_q2 = self.critic_2(state, action)
+            loss_critic_1 = F.mse_loss(current_q1, target_q)
+            loss_critic_2 = F.mse_loss(current_q2, target_q)
+            loss_critic_1.backward()
+            loss_critic_2.backward()
 
-        loss_actor = -self.critic_1(state, actor_action).mean()
-        loss_actor.backward()
-        self.actor_optim.step()
+            self.critic_1_optim.step()
+            self.critic_2_optim.step()
 
-        soft_update(self.target_critic_1, self.critic_1)
-        soft_update(self.target_critic_2, self.critic_2)
-        soft_update(self.target_actor, self.actor)
+            loss_actor = -self.critic_1(state, actor_action).mean()
+            loss_actor.backward()
+            self.actor_optim.step()
+
+            soft_update(self.target_critic_1, self.critic_1)
+            soft_update(self.target_critic_2, self.critic_2)
+            soft_update(self.target_actor, self.actor)
 
     def act(self, state):
         with torch.no_grad():
@@ -142,19 +128,102 @@ class TD3:
         torch.save(self.actor.state_dict(), "agent.pkl")
 
 
-def train():
-    model = TD3(19, 5)
-    suboptimal = np.load("suboptimal.npz", allow_pickle=True)["arr_0"]
-    optimal = np.load("optimal.npz", allow_pickle=True)["arr_0"]
-    dataset = TransitionDataset(np.vstack((optimal, suboptimal)))
-    dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
-    for _ in tqdm.tqdm(range(1000)):
-        for batch in dataloader:
-            model.update(batch)
+class WorldModel(nn.Module):
 
-    return model
+    def __init__(self, state_dim, action_dim, hidden_size):
+        super(WorldModel, self).__init__()
+        self.rnn = nn.LSTM(state_dim + action_dim, hidden_size, bidirectional=False, batch_first=True)
+        self.state_head = nn.Linear(hidden_size, state_dim)
+        self.reward_head = nn.Linear(hidden_size, 1)
+        self.done_head = nn.Sequential(
+            nn.Linear(hidden_size, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, states, actions, length):
+        x = torch.cat((states, actions), dim=-1)
+        packed = nn.utils.rnn.pack_padded_sequence(x, length, batch_first=True, enforce_sorted=False)
+        out_packed, (_, _) = self.rnn(packed)
+        out, _ = nn.utils.rnn.pad_packed_sequence(out_packed, batch_first=True)
+        next_state = self.state_head(out)
+        reward = self.reward_head(out)
+        done = self.done_head(out)
+        return next_state, reward, done
+
+
+class WorldModelEnv:
+
+    def __init__(self):
+        self.model = WorldModel(19, 5, 256)
+        state_dict = torch.load(__file__[:-8] + "/world.pkl", map_location="cpu")
+        self.model.load_state_dict(state_dict)
+        self.h = torch.zeros(1, 256)
+        self.c = torch.zeros(1, 256)
+        self.initial_steps = np.load(__file__[:-8] + "/initial_state.npy")
+        self.current_state = self.choose_state()
+
+    def choose_state(self):
+        idx = np.random.randint(self.initial_steps.shape[0])
+        return torch.from_numpy(self.initial_steps[idx]).view(1, 1, -1).float()
+
+    def reset(self):
+        self.h = torch.zeros(1, 1, 256, dtype=torch.float32)
+        self.c = torch.zeros(1, 1, 256, dtype=torch.float32)
+        self.current_state = self.choose_state()
+        return self.current_state.numpy().ravel()
+
+    def step(self, action):
+        with torch.no_grad():
+            action = torch.from_numpy(action).view(1, 1, -1).float()
+            x = torch.cat((self.current_state, action), dim=-1)
+            out, (h, c) = self.model.rnn(x, (self.h, self.c))
+            next_state = self.model.state_head(out)
+            reward = self.model.reward_head(out)
+            done = self.model.done_head(out)
+            self.h = h
+            self.c = c
+            self.current_state = next_state
+            next_state = next_state.numpy().ravel()
+            reward = reward.item()
+            done = done.item() > 0.5
+        return next_state, reward, done, None
+
+
+def evaluate_policy(env, agent, episodes=5):
+    returns = []
+    for _ in range(episodes):
+        done = False
+        state = env.reset()
+        total_reward = 0.
+
+        while not done:
+            state, reward, done, _ = env.step(agent.act(state))
+            total_reward += reward
+        returns.append(total_reward)
+    return returns
 
 
 if __name__ == "__main__":
-    model = train()
-    model.save()
+    env = WorldModelEnv()
+    test_env = WorldModelEnv()
+    td3 = TD3(state_dim=19, action_dim=5)
+    state = env.reset()
+    episodes_sampled = 0
+    steps_sampled = 0
+
+    for i in range(TRANSITIONS):
+        steps = 0
+
+        # Epsilon-greedy policy
+        action = td3.act(state)
+        action = np.clip(action + EPS * np.random.randn(*action.shape), -ACTOR_CLIP, ACTOR_CLIP)
+
+        next_state, reward, done, _ = env.step(action)
+        td3.update((state, action, next_state, reward, done))
+
+        state = next_state if not done else env.reset()
+
+        if (i + 1) % (TRANSITIONS // 100) == 0:
+            rewards = evaluate_policy(test_env, td3, 5)
+            print(f"Step: {i + 1}, Reward mean: {np.mean(rewards)}, Reward std: {np.std(rewards)}")
+            td3.save()
---
+++ hw4/world_model.py
@@ -0,0 +1,155 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torch import nn
+from torch.optim import Adam
+from torch.utils.data import DataLoader, Dataset
+
+EPOCHES = 200
+LR = 1e-3
+HIDDEN_DIM = 256
+BATCH_SIZE = 4
+DEVICE = "cuda"
+GRAD_NORM = 2.0
+
+
+def collect_data():
+    transictions = []
+    optimal = np.load("suboptimal.npz", allow_pickle=True)["arr_0"]
+    suboptimal = np.load("optimal.npz", allow_pickle=True)["arr_0"]
+
+    for dataset in [optimal, suboptimal]:
+        traj = []
+        for state, action, next_state, reward, done in dataset:
+            if len(traj) > 0 and (state != traj[-1][2]).any():
+                transictions.append(traj)
+                traj = []
+            traj.append((state, action, next_state, reward, done))
+            if done:
+                transictions.append(traj)
+                traj = []
+        if len(traj) > 0:
+            transictions.append(traj)
+    return transictions
+
+
+class TransitionDataset(Dataset):
+
+    def __init__(self, data):
+        self.data = data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        state, action, next_state, reward, done = list(zip(*self.data[idx]))
+        state = np.array(state)
+        action = np.array(action)
+        next_state = np.array(next_state)
+        reward = np.array(reward)
+        done = np.array(done)
+
+        return {
+            "state": state,
+            "action": action,
+            "next_state": next_state,
+            "reward": reward,
+            "done": done
+        }
+
+
+class TransitionCollate:
+
+    def __call__(self, batch):
+        batch_size = len(batch)
+        lengths = [el["state"].shape[0] for el in batch]
+        max_len = max(lengths)
+        states = torch.zeros(batch_size, max_len, batch[0]["state"].shape[1], dtype=torch.float32)
+        actions = torch.zeros(batch_size, max_len, batch[0]["action"].shape[1], dtype=torch.float32)
+        next_states = torch.zeros(batch_size, max_len, batch[0]["next_state"].shape[1], dtype=torch.float32)
+        rewards = torch.zeros(batch_size, max_len, dtype=torch.float32)
+        dones = torch.zeros(batch_size, max_len, dtype=torch.float32)
+
+        for i, element in enumerate(batch):
+            state = element["state"]
+            action = element["action"]
+            next_state = element["next_state"]
+            reward = element["reward"]
+            done = element["done"]
+            states[i, :state.shape[0], :] = torch.from_numpy(state)
+            actions[i, :action.shape[0], :] = torch.from_numpy(action)
+            next_states[i, :next_state.shape[0], :] = torch.from_numpy(next_state)
+            rewards[i, :reward.shape[0]] = torch.from_numpy(reward)
+            dones[i, :done.shape[0]] = torch.from_numpy(done)
+
+        lengths = torch.LongTensor(lengths)
+
+        return {
+            "states": states,
+            "actions": actions,
+            "next_states": next_states,
+            "rewards": rewards,
+            "dones": dones,
+            "lengths": lengths
+        }
+
+
+class WorldModel(nn.Module):
+
+    def __init__(self, state_dim, action_dim, hidden_size):
+        super(WorldModel, self).__init__()
+        self.rnn = nn.LSTM(state_dim + action_dim, hidden_size, bidirectional=False, batch_first=True)
+        self.state_head = nn.Linear(hidden_size, state_dim)
+        self.reward_head = nn.Linear(hidden_size, 1)
+        self.done_head = nn.Sequential(
+            nn.Linear(hidden_size, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, states, actions, length):
+        x = torch.cat((states, actions), dim=-1)
+        packed = nn.utils.rnn.pack_padded_sequence(x, length, batch_first=True, enforce_sorted=False)
+        out_packed, (_, _) = self.rnn(packed)
+        out, _ = nn.utils.rnn.pad_packed_sequence(out_packed, batch_first=True)
+        next_state = self.state_head(out)
+        reward = self.reward_head(out)
+        done = self.done_head(out)
+        return next_state, reward, done
+
+
+if __name__ == "__main__":
+    transitions = collect_data()
+    initial_state = [traj[0][0] for traj in transitions]
+    np.save("initial_state.npy", np.array(initial_state))
+    dataset = TransitionDataset(transitions)
+    dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, collate_fn=TransitionCollate(), shuffle=True)
+    model = WorldModel(19, 5, HIDDEN_DIM).to(DEVICE)
+    optimizer = Adam(model.parameters(), LR)
+    done_criteria = nn.BCELoss()
+    for e in range(EPOCHES):
+        for i, batch in enumerate(dataloader):
+            optimizer.zero_grad()
+            states = batch["states"].to(DEVICE)
+            actions = batch["actions"].to(DEVICE)
+            next_states = batch["next_states"].to(DEVICE)
+            rewards = batch["rewards"].to(DEVICE)
+            dones = batch["dones"].to(DEVICE)
+            lengths = batch["lengths"]
+            next_state_pred, reward_pred, done_pred = model.forward(states, actions, lengths)
+            loss_state = F.mse_loss(next_state_pred, next_states)
+            loss_reward = F.mse_loss(reward_pred.squeeze(2), rewards)
+            loss_done = done_criteria(done_pred.squeeze(2), dones)
+            loss = loss_state + loss_reward + loss_done
+            loss.backward()
+            nn.utils.clip_grad_norm_(model.parameters(), GRAD_NORM)
+            optimizer.step()
+
+            if not (i + 1) % 100:
+                print(
+                    f"epoch: {e + 1}/{EPOCHES}, "
+                    f"iter: {i + 1}/{len(dataloader)}, "
+                    f"loss state: {loss_state.item():.3f} "
+                    f"loss reward: {loss_reward.item():.3f} "
+                    f"loss done: {loss_done.item():.3f} "
+                )
+    torch.save(model.state_dict(), "world.pkl")
